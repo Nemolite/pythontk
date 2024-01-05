a9bb5c3dfa6eee4ca6825251f6b2af9ef3021f6a
Nemolite <g25092011@mail.ru>
2024-01-05 12:51:26 +0300

    Магические методы

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+class Test:
+    def __str__(self):
+        return "str"
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __repr__(self):
+        return "info"
+
+obj = Test()
+print(obj)
+
+print(repr(obj))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
